3ee238497217789454e626bf3e2bcb06e208fe45
robgalestudio <76790147+robgalestudio@users.noreply.github.com>
2021-03-12 17:29:05 -0800

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,27 +1,35 @@
 // Sticky menu
 var new_scroll_position = 0;
 var last_scroll_position;
-var header = document.querySelector('.js-header');
+var header = document.getElementById("js-header");
+var stickyMenu = document.getElementById("js-navbar-menu");
 
 window.addEventListener('scroll', function (e) {
-    last_scroll_position = window.scrollY;
+	last_scroll_position = window.scrollY;
 
-    // Scrolling down
-    if (new_scroll_position < last_scroll_position && last_scroll_position > 40) {
-        header.classList.remove("is-visible");
-        header.classList.add("is-hidden");
+	// Scrolling down
+	if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+		header.classList.remove("is-visible");
+		header.classList.add("is-hidden");
 
-        // Scrolling up
-    } else if (new_scroll_position > last_scroll_position) {
-        header.classList.remove("is-hidden");
-        header.classList.add("is-visible");
-    }
+		// Scrolling up
+	} else if (new_scroll_position > last_scroll_position) {
+		header.classList.remove("is-hidden");
+		header.classList.add("is-visible");
+		if (stickyMenu) {
+			stickyMenu.classList.add("is-sticky");
+		}
+	}
 
-    if (last_scroll_position < 1) {
-        header.classList.remove("is-visible");
-    }
+	if (last_scroll_position < 1) {
+		header.classList.remove("is-visible");
 
-    new_scroll_position = last_scroll_position;
+		if (stickyMenu) {
+			stickyMenu.classList.remove("is-sticky");
+		}
+	}
+
+	new_scroll_position = last_scroll_position;
 });
 
 
@@ -419,12 +427,6 @@ window.addEventListener('scroll', function (e) {
         if (relatedContainer) {
             relatedContainer.classList.remove(config.relatedContainerForOverlayMenuClass);
         }
-
-        var menuOverlay = document.querySelector('.' + config.mobileMenuSidebarOverlayClass);
-
-        if (menuOverlay) {
-            menuOverlay.classList.add(config.hiddenElementClass);
-        }
     }
 
     /**
@@ -433,51 +435,11 @@ window.addEventListener('scroll', function (e) {
     init();
 })(window.publiiThemeMenuConfig);
 
-// Pop-up
+// Share buttons pop-up
 (function () {
-    
-   // close popup
-    var closeHandlers = document.querySelectorAll('.popup .popup-close');
-
-    if (closeHandlers.length) {
-        for (var i = 0; i < closeHandlers.length; i++) {
-            closeHandlers[i].addEventListener('click', function () {
-                this.parentNode.classList.add('is-fade-out');
-
-                setTimeout(() => {
-                    this.parentNode.classList.remove('is-fade-out');
-                    this.parentNode.classList.remove('is-visible');
-                }, 690);
-            });
-        }
-    }
-    
-    // contact popup
-    let contactButtons = document.querySelectorAll('.js-contact-cta');
-    let contactPopup = document.querySelector('.js-contact');
-
-    if (contactButtons.length) {
-        contactPopup.addEventListener('click', function (e) {
-            e.stopPropagation();
-        });
-
-        for (let i = 0; i < contactButtons.length; i++) {
-            contactButtons[i].addEventListener('click', function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-                contactPopup.classList.toggle('is-visible');
-            });
-        }
-
-        document.body.addEventListener('click', function () {
-            contactPopup.classList.remove('is-visible');
-        });
-    }
-    
-    
     // share popup
-    let shareButton = document.querySelector('.js-portfolio-cta');
-    let sharePopup = document.querySelector('.js-portfolio');
+    let shareButton = document.querySelector('.js-post__share-button');
+    let sharePopup = document.querySelector('.js-post__share-popup');
 
     if (shareButton) {
         sharePopup.addEventListener('click', function (e) {
@@ -534,3 +496,104 @@ window.addEventListener('scroll', function (e) {
 })();
 
 
+// Load search input area
+var searchButton = document.querySelector('.js-search-btn');
+    searchOverlay = document.querySelector('.js-search-overlay');
+    searchInput = document.querySelector('.js-search-input');
+
+if (searchButton) {
+    searchButton.addEventListener('click', function () {        
+        searchOverlay.classList.toggle('expanded');
+       
+        setTimeout(function() {
+            if (searchOverlay.classList.contains('expanded')) {
+                searchInput.focus();
+            }
+        }, 60);        
+    });
+
+    searchOverlay.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+
+    searchButton.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+
+    document.body.addEventListener('click', function () {
+        searchOverlay.classList.remove('expanded');
+    });
+}
+
+
+// Back to Top - by CodyHouse.co on MIT license
+(function(){    
+	var backTop = document.getElementsByClassName('js-footer__bttop')[0],		
+		offset = 600,		
+		offsetOpacity = 1200,
+		scrollDuration = 50,
+		scrolling = false;
+	if( backTop ) {		
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
+			}
+		});
+		backTop.addEventListener('click', function(event) {
+			event.preventDefault();
+			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : scrollTop(scrollDuration);
+		});
+	}
+
+	function checkBackToTop() {
+		var windowTop = window.scrollY || document.documentElement.scrollTop;
+		( windowTop > offset ) ? addClass(backTop, 'footer__bttop--show') : removeClass(backTop, 'footer__bttop--show', 'footer__bttop--fade-out');
+		( windowTop > offsetOpacity ) && addClass(backTop, 'footer__bttop--fade-out');
+		scrolling = false;
+	}
+	
+	function scrollTop(duration) {
+	    var start = window.scrollY || document.documentElement.scrollTop,
+	        currentTime = null;
+	        
+	    var animateScroll = function(timestamp){
+	    	if (!currentTime) currentTime = timestamp;        
+	        var progress = timestamp - currentTime;
+	        var val = Math.max(Math.easeInOutQuad(progress, start, -start, duration), 0);
+	        window.scrollTo(0, val);
+	        if(progress < duration) {
+	            window.requestAnimationFrame(animateScroll);
+	        }
+	    };
+
+	    window.requestAnimationFrame(animateScroll);
+	}
+
+	Math.easeInOutQuad = function (t, b, c, d) {
+ 		t /= d/2;
+		if (t < 1) return c/2*t*t + b;
+		t--;
+		return -c/2 * (t*(t-2) - 1) + b;
+	};
+    
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);	
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+})();
